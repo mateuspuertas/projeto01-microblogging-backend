d4973a5da5e367d40a59e1f1a3b69ada82e97df2
mateuspuertas <145804402+mateuspuertas@users.noreply.github.com>
2025-06-01 22:53:45 -0300

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,46 @@
+const logger = require('../utils/logger');
+const { ObjectId } = require('mongodb');
+
 class Post {
   constructor(db) {
     this.collection = db.collection('posts');
   }
 
   async create(title, content, author) {
-    if (!title || !content || !author) throw new Error('Campos obrigatórios faltando!');
-    return await this.collection.insertOne({ title, content, author, createdAt: new Date() });
+    try {
+      if (!title || !content || !author) throw new Error('Campos obrigatórios faltando!');
+      return await this.collection.insertOne({ title, content, author, createdAt: new Date() });
+    } catch (error) {
+      logger.error(error.message);
+      throw error;
+    }
   }
 
   async findAll() {
-    return await this.collection.find().toArray();
+    try {
+      return await this.collection.find().toArray();
+    } catch (error) {
+      logger.error(error.message);
+      throw error;
+    }
+  }
+
+  async update(postId, data) {
+    try {
+      return await this.collection.updateOne({ _id: new ObjectId(postId) }, { $set: data });
+    } catch (error) {
+      logger.error(error.message);
+      throw error;
+    }
+  }
+
+  async delete(postId) {
+    try {
+      return await this.collection.deleteOne({ _id: new ObjectId(postId) });
+    } catch (error) {
+      logger.error(error.message);
+      throw error;
+    }
   }
 }
 
